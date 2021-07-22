e9bc0a6ab875a413e0b44bfbdd4bdb7e2b2eb3ec
qww1552 <qww1552>
2021-07-22 11:42:50 +0900

    first commit

---
+++ src/test/java/com/example/bob_friend/BobFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bob_friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobFriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
