af5629f2b328a65a4d713a88d7301c3099455686
qww1552 <qww1552>
2021-07-29 22:53:52 +0900

    recruitment controller (create, find, find all)

--- src/main/java/com/example/bob_friend/controller/RecruitmentController.java
+++ src/main/java/com/example/bob_friend/controller/RecruitmentController.java
@@ -1,26 +1,41 @@
 package com.example.bob_friend.controller;
 
 import com.example.bob_friend.model.card.domain.Recruitment;
+import com.example.bob_friend.model.card.dto.RecruitmentRequestDto;
+import com.example.bob_friend.model.card.dto.RecruitmentResponseDto;
 import com.example.bob_friend.model.card.exception.RecruitmentNotFoundException;
-import com.example.bob_friend.repository.RecruitmentRepository;
+import com.example.bob_friend.service.RecruitmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/recruitments")
 public class RecruitmentController {
+    private final RecruitmentService recruitmentService;
 
-    private final RecruitmentRepository recruitmentRepository;
+    @GetMapping()
+    public ResponseEntity getAllRecruitment() {
+        List<RecruitmentResponseDto> responseDtoList = recruitmentService.findAll();
+        return ResponseEntity.ok(responseDtoList);
+    }
 
     @GetMapping("/{recruitmentId}")
-    public ResponseEntity getCard(@PathVariable Long recruitmentId) {
-        Recruitment recruitment = recruitmentRepository.findById(recruitmentId).orElseThrow(
-                () -> new RecruitmentNotFoundException()
-        );
-        return ResponseEntity.ok(recruitment);
+    public ResponseEntity getRecruitment(@PathVariable Long recruitmentId) {
+        RecruitmentResponseDto recruitmentResponseDto = recruitmentService.findById(recruitmentId);
+        return ResponseEntity.ok(recruitmentResponseDto);
     }
+
+    @PostMapping()
+    public ResponseEntity createRecruitment(RecruitmentRequestDto recruitmentRequestDto) {
+        RecruitmentResponseDto createdRecruitment = recruitmentService.add(recruitmentRequestDto);
+        return ResponseEntity.ok(createdRecruitment);
+    }
+
+
 
     @ExceptionHandler(value = RecruitmentNotFoundException.class)
     public ResponseEntity handleCardNotFound(RecruitmentNotFoundException exception) {
