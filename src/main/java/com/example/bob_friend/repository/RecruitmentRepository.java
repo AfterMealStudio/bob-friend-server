984e96f96702762e2088480405163dc2fd363a28
qww1552 <qww1552>
2021-08-26 22:58:54 +0900

    식당 기준으로 Recruit 전체 조회

--- src/main/java/com/example/bob_friend/repository/RecruitmentRepository.java
+++ src/main/java/com/example/bob_friend/repository/RecruitmentRepository.java
@@ -3,5 +3,9 @@ package com.example.bob_friend.repository;
 import com.example.bob_friend.model.entity.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+    List<Recruitment> findAllByRestaurantName(String restaurantName);
+
 }
