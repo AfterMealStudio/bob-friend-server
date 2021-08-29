17cb50d3a11c177f119bae107ad7475da3d46e05
qww1552 <qww1552>
2021-08-29 14:52:25 +0900

    로그인, 회원가입 시 주소 마지막에 '/' 허용

--- src/main/java/com/example/bob_friend/configuration/SecurityConfig.java
+++ src/main/java/com/example/bob_friend/configuration/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/signin", "/api/signup").permitAll()
+                .antMatchers("/api/signin/**/", "/api/signup/**/").permitAll()
                 .anyRequest().hasRole("USER")
 
                 .and()
