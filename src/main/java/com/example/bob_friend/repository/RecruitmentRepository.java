f5b39a9ebaedec3efbb49c090aa0f929f8454bab
qww1552 <qww1552>
2021-08-31 22:58:52 +0900

    findAllByAuthor

--- src/main/java/com/example/bob_friend/repository/RecruitmentRepository.java
+++ src/main/java/com/example/bob_friend/repository/RecruitmentRepository.java
@@ -1,5 +1,6 @@
 package com.example.bob_friend.repository;
 
+import com.example.bob_friend.model.entity.Member;
 import com.example.bob_friend.model.entity.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
     List<Recruitment> findAllByRestaurantName(String restaurantName);
 
+    List<Recruitment> findAllByAuthor(Member member);
 }
