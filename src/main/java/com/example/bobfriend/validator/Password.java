baaf50cc3b5b1df9aa3b72dc52d845952d5bccb5
qww1552 <qww1552>
2022-01-13 17:00:03 +0900

    PasswordValidator 에러메시지 변경

--- src/main/java/com/example/bobfriend/validator/Password.java
+++ src/main/java/com/example/bobfriend/validator/Password.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Password {
-    String message() default "패스워드가 적합하지 않습니다";
+    String message() default "{password.message}";
 
     Class<?>[] groups() default {};
 
