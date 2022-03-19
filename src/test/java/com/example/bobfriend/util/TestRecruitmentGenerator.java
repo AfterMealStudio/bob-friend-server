8455733551a8b647f570819847af7ff6aee4fcda
qww1552 <qww1552>
2022-03-19 18:14:12 +0900

    테스트 코드 중복 제거 (RecruitmentCustomRepositoryTest)

--- src/test/java/com/example/bobfriend/util/TestRecruitmentGenerator.java
+++ src/test/java/com/example/bobfriend/util/TestRecruitmentGenerator.java
@@ -4,130 +4,74 @@ import com.example.bobfriend.model.entity.Member;
 import com.example.bobfriend.model.entity.Recruitment;
 import com.example.bobfriend.model.entity.Sex;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 public class TestRecruitmentGenerator {
     private Member testAuthor;
+
     public void setAuthor(Member member) {
         this.testAuthor = member;
     }
 
     public Recruitment getTestRecruitmentWithContent(String content) {
-        return Recruitment.builder()
-                .title("test")
+        return getBaseRecruitmentBuilder()
                 .content(content)
-                .author(testAuthor)
-                .totalNumberOfPeople(4)
-                .sexRestriction(Sex.FEMALE)
-                .restaurantName("test")
-                .restaurantAddress("testRestaurantAddress")
-                .latitude(0.0)
-                .longitude(0.0)
-                .appointmentTime(LocalDateTime.now().plusHours(4))
                 .build();
     }
 
     public Recruitment getTestRecruitmentWithAppointmentTime(LocalDateTime appointmentTime) {
-        return Recruitment.builder()
-                .title("test")
-                .content("test")
-                .author(testAuthor)
-                .totalNumberOfPeople(4)
-                .sexRestriction(Sex.FEMALE)
-                .restaurantName("test")
-                .restaurantAddress("testRestaurantAddress")
-                .latitude(0.0)
-                .longitude(0.0)
+        return getBaseRecruitmentBuilder()
                 .appointmentTime(appointmentTime)
                 .build();
     }
 
-    public Recruitment getTestRecruitmentWithRestaurantName(String restaurantName) {
-        return Recruitment.builder()
-                .title("test")
-                .content("test")
-                .author(testAuthor)
-                .totalNumberOfPeople(4)
-                .sexRestriction(Sex.FEMALE)
-                .restaurantName(restaurantName)
-                .restaurantAddress("testRestaurantAddress")
-                .latitude(0.0)
-                .longitude(0.0)
-                .appointmentTime(LocalDateTime.now().plusHours(4))
-                .build();
-    }
-
     public Recruitment getTestRecruitmentWithRestaurantAddress(String restaurantAddress) {
-        return Recruitment.builder()
-                .title("title 1")
-                .content("content 1")
-                .author(testAuthor)
-                .totalNumberOfPeople(4)
-                .sexRestriction(Sex.FEMALE)
-                .members(Set.of(testAuthor))
-                .restaurantName("testRestaurantName 1")
+        return getBaseRecruitmentBuilder()
                 .restaurantAddress(restaurantAddress)
-                .latitude(0.0)
-                .longitude(0.0)
-                .appointmentTime(LocalDateTime.now().plusHours(4))
                 .build();
     }
 
     public Recruitment getTestRecruitmentWithLocation(double lat, double lon) {
-        return Recruitment.builder()
+        return getBaseRecruitmentBuilder()
                 .latitude(lat)
                 .longitude(lon)
-                .restaurantAddress("test")
-                .author(testAuthor)
-                .appointmentTime(LocalDateTime.now())
-                .sexRestriction(Sex.NONE)
                 .build();
     }
 
     public Recruitment getTestRecruitment() {
+        return getBaseRecruitmentBuilder()
+                .build();
+    }
+
+    public Recruitment getTestRecruitmentWithTitle(String title) {
+        return getBaseRecruitmentBuilder()
+                .title(title)
+                .build();
+    }
+
+    public Recruitment getTestRecruitmentWithSexRestriction(Sex sex) {
+        return getBaseRecruitmentBuilder()
+                .sexRestriction(sex)
+                .build();
+    }
+
+    public Recruitment getTestRecruitmentWithRestaurantName(String restaurantName) {
+        return getBaseRecruitmentBuilder()
+                .restaurantName(restaurantName)
+                .build();
+    }
+
+    private Recruitment.RecruitmentBuilder<?, ?> getBaseRecruitmentBuilder() {
         return Recruitment.builder()
-                .title("title")
-                .content("content")
                 .author(testAuthor)
                 .totalNumberOfPeople(4)
-                .sexRestriction(Sex.FEMALE)
                 .restaurantName("testRestaurantName")
                 .restaurantAddress("testRestaurantAddress")
                 .latitude(0.0)
                 .longitude(0.0)
                 .appointmentTime(LocalDateTime.now().plusHours(4))
-                .build();
-    }
-
-    public Recruitment getTestRecruitmentWithTitle(String title) {
-        return Recruitment.builder()
-                .title(title)
-                .content("test")
-                .author(testAuthor)
-                .totalNumberOfPeople(4)
                 .sexRestriction(Sex.FEMALE)
-                .restaurantName("test")
-                .restaurantAddress("testRestaurantAddress")
-                .latitude(0.0)
-                .longitude(0.0)
-                .appointmentTime(LocalDateTime.now().plusHours(4))
-                .build();
-    }
-
-    public Recruitment getTestRecruitmentWithSexRestriction(Sex sex) {
-        return Recruitment.builder()
-                .title("test")
-                .content("content")
-                .author(testAuthor)
-                .totalNumberOfPeople(4)
-                .sexRestriction(sex)
-                .restaurantName("test")
-                .restaurantAddress("testRestaurantAddress")
-                .latitude(0.0)
-                .longitude(0.0)
-                .appointmentTime(LocalDateTime.now().plusHours(4))
-                .build();
+                .content("testContent")
+                .title("testTitle");
     }
 }
